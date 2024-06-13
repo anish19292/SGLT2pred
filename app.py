75e5242b3ba3e458a0576f0f2b275f676d7cd781
Anish Gomatam <119532203+anish19292@users.noreply.github.com>
2024-06-13 12:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-import streamlit as st
-import os
-import pickle
-import pandas as pd
-from PIL import Image
-from rdkit import Chem
-from rdkit.Chem import AllChem, Draw
-from padelpy import padeldescriptor
+
