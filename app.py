cb9c936ff9c1315a2f6f33a6a8ccc1097849e321
Anish Gomatam <119532203+anish19292@users.noreply.github.com>
2024-06-13 15:44:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-
+load packages
+import streamlit as st
+import requests
+from streamlit_option_menu import option_menu
+from rdkit import Chem
+from rdkit.Chem import Draw
+from mordred import Calculator, descriptors
+from supervised.automl import AutoML
+import pandas as pd
+import numpy as np
+import time
+import hashlib
+import json
+import html
+from streamlit.components.v1 import html
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from PIL import Image
+from streamlit_ketcher import st_ketcher
+import matplotlib.pyplot as plt
+import smtplib
+from email.message import EmailMessage
